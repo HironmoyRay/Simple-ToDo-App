e8a62d9681e4ba5a0fffd8b9a199a2901e9511e3
hironmoy-ray <hpray35@gmail.com>
2024-11-27 19:24:28 +0600

    set Input value using state in addTask

--- src/components/addToTask.jsx
+++ src/components/addToTask.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 export default function AddToTask() {
+  const [text, setText] = useState(" ");
   return (
     <>
-      <input type="text" placeholder="Add your Task" />
+      <input
+        type="text"
+        placeholder="Add your Task"
+        value={text}
+        onChange={(e) => {
+        //   console.log(e.target.value);
+          setText(e.target.value);
+        }}
+      />
       <button>Add Task</button>
     </>
   );
